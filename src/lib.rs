53d1ac6608df978a521497cdca675165c969a8aa
Tali Auster <me@tali.network>
2025-08-07 16:07:20 -0600

    refactor: use trait objects for animation states

--- src/lib.rs
+++ src/lib.rs
@@ -7,14 +7,34 @@ use egui::{self, NumExt, Pos2, Rect, Vec2, widgets};
 mod images;
 use images::*;
 
-/// The [Neko] struct stores the state of the cat such that it can follow the cursor appropriately.
+/// This struct is used as a handle on a cat that may be drawn to follow a cursor. See examples for
+/// usage.
 pub struct Neko {
-    pos: Pos2,
-    last_cursor_pos: Pos2,
-    /// Speed of the cat in pixels/frame.
-    speed: f32,
-    state: usize,
-    ticker: usize,
+    bookkeeping: NekoBookkeeping,
+    animation: Box<dyn Animation>,
+}
+
+/// The NekoBookkeeping struct stores the state of the cat such that it can follow the cursor
+/// appropriately.
+struct NekoBookkeeping {
+    pub pos: Pos2,
+    pub last_cursor_pos: Pos2,
+    /// Speed of the cat in pixels/update.
+    pub speed: f32,
+    pub ticker: usize,
+}
+
+trait Animation {
+    /// Draw the current animation frame or request that a different animation be used.
+    fn display(
+        &mut self,
+        ui: &mut egui::Ui,
+        books: &mut NekoBookkeeping,
+    ) -> Option<Box<dyn Animation>>;
+}
+
+struct SleepingNeko;
+struct RunningNeko {
     direction: Direction,
 }
 
@@ -22,49 +42,84 @@ impl Neko {
     /// Create a new cat.
     pub fn new() -> Self {
         return Self {
-            pos: (0f32, 0f32).into(),
-            last_cursor_pos: (0f32, 0f32).into(),
-            speed: 12f32,
-            state: 0,
-            ticker: 0,
-            direction: Direction::RIGHT, // TODO: sleeping, wake animation
+            bookkeeping: NekoBookkeeping {
+                pos: (0f32, 0f32).into(),
+                last_cursor_pos: (0f32, 0f32).into(),
+                speed: 12f32,
+                ticker: 0,
+            },
+            animation: Box::new(RunningNeko {
+                direction: Direction::RIGHT, // TODO: sleeping, wake animation
+            }),
         };
     }
 
     /// Draw the cat to the ui.
     pub fn draw(&mut self, ui: &mut egui::Ui) {
-        self.ticker = self.ticker.wrapping_add(1);
+        self.bookkeeping.ticker = self.bookkeeping.ticker.wrapping_add(1);
 
-        // Don't move on each render.
         if let Some(cursor_pos) = ui.input(|state| state.pointer.latest_pos()) {
-            self.last_cursor_pos = cursor_pos;
+            self.bookkeeping.last_cursor_pos = cursor_pos;
         };
 
-        if self.last_cursor_pos.distance(self.pos) < self.speed * 3. {
-            self.display_sitting(ui);
-        } else {
-            if self.ticker % 10 == 0 {
-                self.direction = self.step();
-            }
-            self.display_running(ui, self.direction);
+        while let Some(animation) = self.animation.display(ui, &mut self.bookkeeping) {
+            self.animation = animation;
         }
     }
+}
 
-    /// Move one frame in the correct direction.
-    fn step(&mut self) -> Direction {
-        let direction = (self.last_cursor_pos - self.pos).angle();
-        self.pos += Vec2::angled(direction) * self.speed;
+impl Animation for SleepingNeko {
+    fn display(
+        &mut self,
+        ui: &mut egui::Ui,
+        books: &mut NekoBookkeeping,
+    ) -> Option<Box<dyn Animation>> {
+        // If the cursor has gotten far enough away, wake up.
+        if books.last_cursor_pos.distance(books.pos) >= books.speed * 3. {
+            return Some(Box::new(RunningNeko {
+                // Since the first thing that happens in the RunningNeko `display` call is to set
+                // the direction, we're fine just setting this arbitrarily.
+                direction: Direction::RIGHT,
+            }));
+        }
 
-        self.state = self.state.wrapping_add(1);
-        Direction::from_angle(direction)
+        let min = books.pos;
+        let max = (min + Vec2::new(32f32, 32f32)).at_most(ui.max_rect().max);
+        let image = WASH_IMAGES[(books.ticker / 10) % 2].clone();
+        ui.put(Rect { min, max }, widgets::Image::new(image));
+        None
     }
+}
 
-    /// Draw ourselves onto the screen. This is a helper method for [draw].
-    fn display_running(&mut self, ui: &mut egui::Ui, direction: Direction) {
-        let min = self.pos;
+impl RunningNeko {
+    /// Move one frame in the correct direction.
+    fn step(&mut self, books: &mut NekoBookkeeping) {
+        let direction = (books.last_cursor_pos - books.pos).angle();
+        books.pos += Vec2::angled(direction) * books.speed;
+        self.direction = Direction::from_angle(direction)
+    }
+}
+
+impl Animation for RunningNeko {
+    fn display(
+        &mut self,
+        ui: &mut egui::Ui,
+        books: &mut NekoBookkeeping,
+    ) -> Option<Box<dyn Animation>> {
+        // If we're close enough to the cursor, go to sleep.
+        if books.last_cursor_pos.distance(books.pos) < books.speed * 3. {
+            return Some(Box::new(SleepingNeko));
+        }
+
+        // Don't move on each frame.
+        if books.ticker % 10 == 0 {
+            self.step(books);
+        }
+
+        let min = books.pos;
         let max = (min + Vec2::new(32f32, 32f32)).at_most(ui.max_rect().max);
 
-        let image = match direction {
+        let image = match self.direction {
             Direction::RIGHT => &RIGHT_IMAGES,
             Direction::LEFT => &LEFT_IMAGES,
             Direction::DOWN => &DOWN_IMAGES,
@@ -73,17 +128,11 @@ impl Neko {
             Direction::UP => &UP_IMAGES,
             Direction::UPLEFT => &UPLEFT_IMAGES,
             Direction::UPRIGHT => &UPRIGHT_IMAGES,
-        }[self.state % 2]
+        }[(books.ticker / 10) % 2]
             .clone(); // Cloning looks bad, but internally, an ImageSource will utilize Cows, so it should be okay.
 
         ui.put(Rect { min, max }, widgets::Image::new(image));
-    }
-
-    fn display_sitting(&mut self, ui: &mut egui::Ui) {
-        let min = self.pos;
-        let max = (min + Vec2::new(32f32, 32f32)).at_most(ui.max_rect().max);
-        let image = WASH_IMAGES[self.state % 2].clone();
-        ui.put(Rect { min, max }, widgets::Image::new(image));
+        None
     }
 }
 
